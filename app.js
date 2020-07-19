d9e39693f1b6759fa9cded7fccb1375e3b9ecfcb
amrit180 <62633596+amrit180@users.noreply.github.com>
2020-07-19 12:43:57 +0530

    navbar

---
+++ app.js
@@ -0,0 +1,32 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+const company = document.querySelector('#company');
+const logo = document.querySelector('#logo');
+
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = '#29323c';
+        company.style.display = 'block';
+        logo.style.display = 'none';
+    } else {
+        header.style.backgroundColor = 'transparent';
+        company.style.display = 'none';
+        logo.style.display = 'block';
+    }
+});
+
+menu_item.forEach((item) => {
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    });
+});
